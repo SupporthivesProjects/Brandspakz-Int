2413106843cdde22306ed8eb4468517710f66a79
Rohit <rohit@supporthives.com>
2025-04-24 11:50:47 +0530

    changes

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -27,12 +27,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('frontend/Lingosphere/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/Lingosphere/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/Lingosphere/css/main.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/Lingosphere/css/m_styles.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/Lingosphere/css/tirthak_styles.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/Lingosphere/css/saakshi.css') }}">
+    <link rel="stylesheet" href="{{ ('frontend/BrandSparkz/assets/dist/bootstrap/css/bootstrap.min.css') }}">
+
+    
+    <link rel="stylesheet" href="{{ ('frontend/BrandSparkz/assets/dist/owl-carousel/css/owl.carousel.css') }}">
 
     <link rel="stylesheet" href="{{ ('frontend/BrandSparkz/assets/css/m_style.css') }}">
     <link rel="stylesheet" href="{{ ('frontend/BrandSparkz/assets/css/assets/css/sk_style.css') }}">
@@ -500,18 +498,21 @@
     @include('frontend.inc.footer')
     <!-- Essential JavaScript -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script src="{{ asset('frontend/Lingosphere/dist/js/owl.carousel.js') }}"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-    </script>
+    
+
+   
+    <script src="{{ ('frontend/BrandSparkz/assets/dist/bootstrap/js/bootstrap.popper.min.js') }}"></script>
+    <script src="{{ ('frontend/BrandSparkz/assets/dist/bootstrap/js/bootstrap.min.js') }}"></script>
+    
+    <!-- Owl Carousel JS Links -->
+    <script src="{{ ('frontend/BrandSparkz/assets/dist/owl-carousel/js/owl.carousel.js') }}"></script>
+    
+    <!-- JS Link -->
+    <script src="{{ ('frontend/BrandSparkz/assets/js/main.js') }}"></script>
+
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
