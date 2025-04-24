761ff5daacc1509d70b3172f9191fa452df8944a
Rohit <rohit@supporthives.com>
2025-04-24 11:59:34 +0530

    add

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -27,7 +27,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ ('public/frontend/BrandSparkz/assets/dist/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    {{-- <link rel="stylesheet" href="{{ ('public/frontend/BrandSparkz/assets/dist/bootstrap/css/bootstrap.min.css') }}">--}}
 
     
     <link rel="stylesheet" href="{{ ('public/frontend/BrandSparkz/assets/dist/owl-carousel/css/owl.carousel.css') }}">
