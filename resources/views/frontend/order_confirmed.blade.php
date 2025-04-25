5e5f1dc517726e07f6f933caf393967559142bf7
Rohit <rohit@supporthives.com>
2025-04-25 17:10:59 +0530

    changes

--- resources/views/frontend/order_confirmed.blade.php
+++ resources/views/frontend/order_confirmed.blade.php
@@ -20,46 +20,7 @@
         @endphp
     @endif
 
-    {{-- <div class="cf_div">
-
-        <div class="left_cf">
-
-            <img src="{{ asset('frontend/Lingosphere/img/cf_right_sound.svg') }}" alt=""
-                class="img-fluid cf_right_sound mobile_none">
-            <img src="{{ asset('frontend/Lingosphere/img/cf_left_sound.svg') }}" alt=""
-                class="img-fluid cf_left_sound mobile_none">
-
-            <div class="in_left_cf">
-
-                <h1 class="cf_tt">
-                    Payment Successful!
-                </h1>
-
-                <p class="cs_pp">Congratulations, your payment was successful! Thank you for your order.
-                </p>
-                <div class="btn_div">
-
-                    <button class="btn cf_leftbtn forphone_100width" onclick="window.location.href='{{ route('home') }}'">
-                        Return home
-                    </button>
-
-                    <button class="btn cf_rightbtn forphone_100width"
-                        onclick="window.location.href='{{ route('dashboard') }}'">
-                        View Order
-                    </button>
-
-
-
-                </div>
-            </div>
-
-        </div>
-
-        <div class="right_cs">
-
-        </div>
-
-    </div>--}}
+    
 
 
     <section class="pay_S">
